6636e66c6825e4c34e69c7d407d12bf597475533
Ngnawen Samuel <ngnawens@gmail.com>
2025-12-08 21:37:21 -0500

    Combined all changes: first and second local commits with remote merge

---
+++ src/main/java/cm/belrose/demogitcommandrepo/DemoGitCommandRepoApplication.java
@@ -0,0 +1,13 @@
+package cm.belrose.demogitcommandrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitCommandRepoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DemoGitCommandRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cm/belrose/demogitcommandrepo/DemoGitCommandRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.belrose.demogitcommandrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitCommandRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
